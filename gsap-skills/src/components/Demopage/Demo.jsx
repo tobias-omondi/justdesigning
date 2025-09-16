fcb160594cf4c03ecb1e3612e8730d9058ec6105
tobias <tobias.omondi@student.moringaschool.com>
2025-09-16 09:49:47 +0300

    gsap transforms

--- gsap-skills/src/components/Demopage/Demo.jsx
+++ gsap-skills/src/components/Demopage/Demo.jsx
@@ -5,11 +5,19 @@ const Demo = () => {
 
   useEffect (() =>{
     gsap.to (".box", 
-      {x: 300 , duration: 1.2, rotate: 360,
+      {x: 300 , duration: 2, repeat: 2, yoyo: true, scaleY: 0.8 , rotation: "1.90rad", skewY: 30, skewX: 30
      });
 
     gsap.to (".circle", 
-      {x: 300, duration: 2, rotate: 360 ,}
+      {x: 300, duration: 2, rotation: 360, repeat: 2 , yoyo: true , }
+    )
+    gsap.fromTo (".circleshape",
+      {x: 40 , delay : 1.2, autoAlpha: 0 , backgroundColor: 'red'},
+      {x: 150, rotate: 360, skewY: 10 ,duration: 3 , repeat: 2 , yoyo: true ,transformOrigin: "center 40%", autoAlpha: 1, backgroundColor: '#9d95ff', 
+        attr: {
+          rx: 50,
+        }
+       }
     )
   })
   return (
@@ -22,6 +30,8 @@ const Demo = () => {
      {/* circle animation */}
 
      <div className='circle w-20 h-20 bg-amber-400 mt-5 rounded-full shadow-xl'></div>
+
+     <div className='circleshape w-20 h-20 bg-red-500 mt-5 rounded-full shadow-2xl'></div>
     </div>
   )
 }
