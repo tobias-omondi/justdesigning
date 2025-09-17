ff5a2ebe8dbf8adca5a4f357d67ef52b8756a72a
tobias <tobias.omondi@student.moringaschool.com>
2025-09-17 09:56:44 +0300

    gsap power easing

--- gsap-skills/src/components/Easing/Ease.jsx
+++ gsap-skills/src/components/Easing/Ease.jsx
@@ -6,27 +6,27 @@ const Ease = () => {
 
   useEffect (() =>{
     gsap.to (".squire", 
-      {x: 500, rotation: 360 , ease: "power1.out", duration: 3})
+      {x: 500, rotation: 360 , ease: "power1.out", duration: 3 , yoyo: true, repeat: 2})
     gsap.to (".squireone",
-      {x:500, rotation: 360 , ease: "power1.in", duration : 3}
+      {x:500, rotation: 360 , ease: "power1.in", duration : 3, yoyo: true , repeat: 2}
      )
      gsap.to (".squiretwo",
-      {x:500, rotation: 360 , ease: "power1.inOut", duration : 3}
+      {x:500, rotation: 360 , ease: "power1.inOut", duration : 3, yoyo: true, repeat: 2}
      )
      gsap.to (".squirethree",
-      {x:500, rotation: 360 , ease: "power2.Out", duration : 3}
+      {x:500, rotation: 360 , ease: "power2.Out", duration : 3, yoyo: true, repeat: 2}
      )
      gsap.to (".squirefour",
-      {x:500, rotation: 360 , ease: "power3.out", duration : 3}
+      {x:500, rotation: 360 , ease: "power3.out", duration : 3, yoyo: true, repeat: 2}
      )
      gsap.to (".squirefive",
-      {x:500, rotation: 360 , ease: "power4.out", duration : 3}
+      {x:500, rotation: 360 , ease: "power4.out", duration : 3, yoyo: true, repeat: 2}
      )
   }, [])
 
   return (
     <div>
-      <h1 className='font-[clash] mt-5 underline text-2xl'>LEARNING EASING DATE 17</h1>
+      <h1 className='font-[clash] mt-5 underline text-2xl'>LEARNING Power EASING DATE 17</h1>
       <div className='squire bg-yellow-500 w-20 h-20 rounded-xl mt-5'></div>
       <div className='squireone bg-blue-500 w-20 h-20 rounded-xl mt-5'></div>
       <div className='squiretwo bg-red-500 w-20 h-20 rounded-xl mt-5'></div>
